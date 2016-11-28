87f02a8192adb1fbd8501581585d420b5dbb5bdf
shalzuth <shalzuth@gmail.com>
2016-11-27 16:42:16 -0800

    fix case name 2

---
+++ BattleSharpController/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using UnityEngine;
+using BloodGUI_Binding.Base;
+
+namespace BattleSharpController
+{
+    public static class Loader
+    {
+        public static GameObject obj;
+        public static void Load()
+        {
+            //UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+
+            obj = new GameObject();
+            obj.name = "BattleSharp";
+            obj.AddComponent<Menu>();
+            obj.AddComponent<Controller>();
+            GameObject.DontDestroyOnLoad(obj);
+        }
+    }
+}
